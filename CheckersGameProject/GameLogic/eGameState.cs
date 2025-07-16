774c91c20eca200122f66e4ecb123feb64074e60
Hadar2001 <hadar1056@gmail.com>
2025-07-16 14:43:29 +0300

    Initial commit - CheckersGame project

---
+++ CheckersGameProject/GameLogic/eGameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGameProject
+{
+    public enum eGameState
+    {
+        InProgress,
+        Player1Wins,
+        Player2Wins,
+        Draw,
+    }
+}
